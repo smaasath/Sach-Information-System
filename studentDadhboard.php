ed18efeb7893e09e3dd3423fa05ab8194e0e97b0
smaasath <99379342+smaasath@users.noreply.github.com>
2023-06-25 18:10:00 +0530

    nav bar change

--- studentDadhboard.php
+++ studentDadhboard.php
@@ -84,7 +84,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <nav class="navbar navbardah flex-column">
             <div class="container navcon" style="height:30px;">
 
-                <i class="fa-solid fa-user fa-2xl navuser" style="color: #DAFFFB;"></i>
+                <i class="fa-solid fa-user fa-2xl navuser" style="color: #001C30"></i>
                 <div class="name">User Name</div>
 
             </div>
