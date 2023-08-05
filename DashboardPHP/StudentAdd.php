41ddf0e7e3c452915010b14b7697bcd686d1a4f6
smaasath <99379342+smaasath@users.noreply.github.com>
2023-08-06 02:44:07 +0530

    password sending via mail

--- DashboardPHP/StudentAdd.php
+++ DashboardPHP/StudentAdd.php
@@ -1,28 +1,36 @@
 <?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $instituteName = $_POST["instituteName"];
-    $Instituteid = $_POST["Instituteid"];
-    $degree = $_POST["degree"];
-    $studentName = $_POST["studentName"];
-    $entrlmentNumber = $_POST["entrlmentNumber"];
-    $accedamicYear = $_POST["accedamicYear"];
-    $DOB = $_POST["DOB"];
-    $studentContactNum = $_POST["studentContactNum"];
-    $studentEmail = $_POST["studentEmail"];
-    $gender = $_POST["gender"];
-    $studentAddress = $_POST["studentAddress"];
-    $userName = $_POST["userName"];
-    $password = $_POST["password"];
-    $guardianName = $_POST["guardianName"];
-    $guardianRelation = $_POST["guardianRelation"];
-    $guardianContactNum = $_POST["guardianContactNum"];
-    $guardianEmail = $_POST["guardianEmail"];
-    $guardianAddress = $_POST["guardianAddress"];
-    $guardianOccupation = $_POST["guardianOccupation"];
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function sanitizeInput($input) {
+    return htmlspecialchars(trim($input));
 }
 
-echo $instituteName;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $instituteName = sanitizeInput($_POST["instituteName"]);
+    $Instituteid = sanitizeInput($_POST["Instituteid"]);
+    $degree = sanitizeInput($_POST["degree"]);
+    $studentName = sanitizeInput($_POST["studentName"]);
+    $entrlmentNumber = sanitizeInput($_POST["entrlmentNumber"]);
+    $accedamicYear = sanitizeInput($_POST["accedamicYear"]);
+    $DOB = sanitizeInput($_POST["DOB"]);
+    $studentContactNum = sanitizeInput($_POST["studentContactNum"]);
+    $studentEmail = sanitizeInput($_POST["studentEmail"]);
+    $gender = sanitizeInput($_POST["gender"]);
+    $studentAddress = sanitizeInput($_POST["studentAddress"]);
+    $userName = sanitizeInput($_POST["userName"]);
+    $password = sanitizeInput($_POST["password"]);
+    $guardianName = sanitizeInput($_POST["guardianName"]);
+    $guardianRelation = sanitizeInput($_POST["guardianRelation"]);
+    $guardianContactNum = sanitizeInput($_POST["guardianContactNum"]);
+    $guardianEmail = sanitizeInput($_POST["guardianEmail"]);
+    $guardianAddress = sanitizeInput($_POST["guardianAddress"]);
+    $guardianOccupation = sanitizeInput($_POST["guardianOccupation"]);
+}
+$passwordch = htmlentities($password);
+echo $instituteName, $passwordch;
 
 include '../DashboardPHP/connection.php';
 
@@ -35,11 +43,11 @@ if (mysqli_query($conn, $sqlStudent)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-$sqlgurdian ="INSERT INTO `guardian` (`guardianId`, `studentID`, `name`, `relationShip`, `email`, `phoneNo`, `address`, `occupation`) "
+$sqlgurdian = "INSERT INTO `guardian` (`guardianId`, `studentID`, `name`, `relationShip`, `email`, `phoneNo`, `address`, `occupation`) "
         . "VALUES (NULL, '$student_id', '$guardianName', '$guardianRelation', '$guardianEmail', '$guardianContactNum', '$guardianAddress', '$guardianOccupation')";
 
 if (mysqli_query($conn, $sqlgurdian)) {
-   
+
     echo "guadian created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
@@ -53,80 +61,61 @@ if (mysqli_query($conn, $sqlgurdian)) {
 
 
 <html>
-  <head>
-    <title>Test</title>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-  </head>
-  <body>
-    
+    <head>
+        <title>Test</title>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    </head>
+    <body>
 
-<?php
+
+        <?php
+
 //Import PHPMailer classes into the global namespace
 //These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\SMTP;
+        use PHPMailer\PHPMailer\Exception;
 
 require '../mail/Exception.php';
-require '../mail/PHPMailer.php';
-require '../mail/SMTP.php';
-
-
-
-
-
-
+        require '../mail/PHPMailer.php';
+        require '../mail/SMTP.php';
 
 //Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
+        $mail = new PHPMailer(true);
 //ast_123_
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'sachinformationsystem@gmail.com';                     //SMTP username
-    $mail->Password   = 'upyjmbtlcfckzoke';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+            $mail->Username = 'sachinformationsystem@gmail.com';                     //SMTP username
+            $mail->Password = 'upyjmbtlcfckzoke';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            //Recipients
+            $mail->setFrom('aststore39@gmail.com');
+            $mail->addAddress($studentEmail);     //Add a recipient             //Name is optional
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'Student Registration';
+            $message = "Dear " . $studentName . ",<br><br>";
+            $message .= "You are registered into " . $instituteName . " via Sachini Information System.<br>";
+            $message .= "Your password is: " . $passwordch;
 
-    //Recipients
-    $mail->setFrom('aststore39@gmail.com');
-    $mail->addAddress($studentEmail);     //Add a recipient             //Name is optional
+            $mail->Body = $message;
 
+            $mail->send();
+            ?>
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Student Registration';
-    $mail->Body    = "Dear ".$studentName.","."<br> You Are Register into".$instituteName." Via Sachini Information System"
-            . "<br> Here Your Details are Attached which are ebtered by the".$instituteName." Admin"
-            . "<br> Name:".$studentName.""
-            . "<br> Entrollment Number:".$entrlmentNumber.""
-            . "<br> DOB:".$DOB.""
-            . "<br> Gender:".$gender.""
-            . "<br> Address:".$studentAddress.""
-            . "<br> Phone Number:".$studentContactNum.""
-            . "<br> User Name:".$userName.""
-            . "<br> Password:".$password.""
-            . "<br>"
-            . "If you Need Any changes On this Details Please Contact Your University Admin"
-            . "<br> ThankYou!"
-            . "<br> Regards,"
-            . "<br> Sach Information System";
+            <?php
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            ?>
 
-    $mail->send();
-    ?>
-   
-    <?php
+            <?php
+        }
+        ?>
 
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    ?>
-   
-    <?php
-}
-?>
-
-  </body>
+    </body>
 </html>
