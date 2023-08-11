b83dca42be1bb7121e1fe6b7c3942f190df18988
Gimhani2000 <86472955+Gimhani2000@users.noreply.github.com>
2023-08-11 20:44:46 +0530

    update super admin profile edit

--- DashboardFiles/SuperAdminProfileEdit.php
+++ DashboardFiles/SuperAdminProfileEdit.php
@@ -1,3 +1,10 @@
+<?php
+require '../classes/DBConnector.php';
+
+use classes\DBConnector;
+
+$dbcon = new DBConnector();
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -9,66 +16,106 @@
         <link rel="stylesheet" href="../Profile/Admin-form/style.css">
     </head>
     <body>
-        
-            <div class="container py-md-5">        
-                        <div class="card shadow mb-3">
-                            <div class="card-header py-3 text-center">
-                                <p class="m-0 fw-bold">ADMIN PROFILE</p>
-                            </div>
-                            <form>
-                            <div class="card-body">                                
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="Department"><strong>Department</strong></label><input class="form-control" type="text" id="Department" placeholder="Computer science and infomatics" name="Department" readonly></div>
-                                        </div>
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="Degree"><strong>Degree</strong></label><input class="form-control" type="text" id="Degree" placeholder="Computer science and technology" name="Degree" readonly></div>
-                                        </div>
-                                    </div>                                
-                            </div>
-                            <div class="card-body">                                
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="Name"><strong>Name</strong></label><input class="form-control" type="text" id="Name-4" placeholder="D.G.S.Pathulpana" name="Name"></div>
-                                        </div>
-                                    </div>                                
-                            </div>
-                            <div class="card-body">                                
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="tel"><strong>Contact</strong></label></div><input class="form-control" type="tel" placeholder="0777267345">
-                                        </div>
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="email"><strong>Email Address</strong></label></div><input class="form-control" type="email" id="email-1" placeholder="aaa@gmail.com" name="email">
-                                        </div>
-                                    </div>                                
-                            </div>
-                            <div class="card-body">                                
-                                    <div class="row">
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="User"><strong>User name</strong></label></div><input class="form-control" type="text" placeholder="ADMIN043">
-                                        </div>
-                                        <div class="col">
-                                            <div class="mb-3"><label class="form-label" for="password"><strong>Password</strong></label></div><input class="form-control" type="password">
-                                        </div>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $uderId = ($_POST['uderId']);
+            $userName = ($_POST['userName']);
+            $email = ($_POST['email']);          
+
+            try {
+                $con = $dbcon->getConnection();
+                $query = "UPDATE user SET userName=?, email=? WHERE `user`.`uderId` = ?";
+
+                $pstmt = $con->prepare($query);
+                $pstmt->bindValue(1, $userName);
+                $pstmt->bindValue(2, $email);
+                $pstmt->bindValue(3, $uderId);                
+                $pstmt->execute();
+            
+                if ($pstmt->rowCount() > 0){
+                    header("Location: ../DashboardFiles/SuperAdminProfile.php");
+                    exit;
+                }
+                //If user didn't do any changes but clicked update button.
+                else {
+                    header("Location: ../DashboardFiles/SuperAdminProfile.php");
+                    exit;
+                }
+            } catch (PDOException $exc) {
+                echo $exc->getMessage();
+            }
+        } else {
+
+            $uderId = $_GET["id"];
+            try {
+                $con = $dbcon->getConnection();
+                $query = "SELECT * FROM user WHERE uderId=?";
+                $pstmt1 = $con->prepare($query);
+                $pstmt1->bindValue(1, $uderId);
+                $pstmt1->execute();
+                $rs1 = $pstmt1->fetch(PDO::FETCH_OBJ);
+
+            } catch (PDOException $exc) {
+                echo $exc->getMessage();
+            }
+            ?> 
+            <div class="container py-md-5">
+                <div class="card shadow mb-3">
+                    <div class="card-header py-3 text-center">
+                        <p class="m-0 fw-bold ">ADMIN PROFILE</p>
+                    </div>            
+                    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <input type="hidden" name="uderId" value="<?php echo $uderId; ?>"/>
                                     </div>
-                                    <br>
-                                    <button class="Submit-Btn" type="button" onclick="savestudent()">Save Changes</button>
+                                </div>
                             </div>
-                        </form>
+                        </div>               
+                        <div class="card-body">                                
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3"><label class="form-label" ><strong>User Name</strong></label><input class="form-control" name="userName" type="text" value="<?php echo $rs1->userName; ?>"></div>
+                                </div>                              
+                            </div>
+                            <div class="mb-3"></div>                                
+                        </div>          
+                        <div class="card-body">                                
+                            <div class="row">                             
+                                <div class="col">
+                                    <div class="mb-3"><label class="form-label" for="User"><strong>Email Address</strong></label></div><input class="form-control" name="email" type="email" value="<?php echo $rs1->email; ?>">
+                                </div>                                        
+                            </div>
+                            <div class="mb-3"></div>
                         </div>
-                    </div>               
-               </div>
-            </div>  
-            <script src="../Profile/Admin-form/validation.js" type="text/javascript"></script>
-         <script src="../../JS/AdminDashboard.js"></script>
+                        <div class="card-body">                                
+                            <div class="row">
+                                <div class="col">
+                                    <button class="Submit-Btn" type="submit">Update</button>
+                                </div>
+                                <div class="col">
+                                    <a href="../DashboardFiles/SuperAdminProfile.php">
+                                        <button class="Submit-Btn" type="button" >Cancel</button>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="mb-3"></div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        <?php } ?> 
+        
     </body>
 </html>
 
 
- 
 
 
- 
+
+
 
 
